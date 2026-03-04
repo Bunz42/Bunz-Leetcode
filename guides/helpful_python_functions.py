9c8acb4a62eccf6410f7140003234949ee1e81f7
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-03-03 21:38:49 -0500

    Update helpful_python.py

--- guides/helpful_python_functions.py
+++ guides/helpful_python_functions.py
@@ -1,1 +1,11 @@
-
+.isalnum() # Checks if a character is alphanumeric
+.lower() # Lowers the case of a string
+.sort() # Sorts in ascending order
+.sort(reverse=True) # Sorts in descending order
+max() # Finds the max between two numbers
+min() # Finds the min between two numbers
+len() # Finds the length of an iterable
+set() # Initializes a set in python
+Counter() # Counts hashable objects
+collections.deque() # Monotonic queue
+zip() # Links two lists by storing their elements in tuples
