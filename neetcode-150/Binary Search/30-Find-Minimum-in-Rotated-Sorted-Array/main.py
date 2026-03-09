2df0112ecd835a4c0212178e02f755424fd46694
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-03-08 20:44:52 -0400

    Implement findMin function to locate minimum in rotated array

--- neetcode-150/Binary Search/30-Find-Minimum-in-Rotated-Sorted-Array/main.py
+++ neetcode-150/Binary Search/30-Find-Minimum-in-Rotated-Sorted-Array/main.py
@@ -1,19 +1,14 @@
 class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        min_k = r
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
 
-        while l <= r:
-            k = (l + r) // 2
-            time = 0
+        while l < r:
+            mid = (l + r) // 2
 
-            for x in piles:
-                time += math.ceil(x/k)
-            
-            if time <= h:
-                min_k = min(min_k, k)
-                r = k - 1
+            if nums[mid] > nums[r]:
+                l = mid + 1
             else:
-                l = k + 1
+                r = mid
         
-        return min_k
+        return nums[l]
