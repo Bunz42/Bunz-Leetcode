5ca1742751a0b16bf3996f9132a2a349f56d9e7a
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-03-10 15:13:49 -0400

    Implement search in rotated sorted array
    Refactored the binary search logic to include target search in a rotated sorted array.

--- neetcode-150/Binary Search/31-Search-in-Rotated-Sorted-Array/main.py
+++ neetcode-150/Binary Search/31-Search-in-Rotated-Sorted-Array/main.py
@@ -1,14 +1,23 @@
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
+    def search(self, nums: List[int], target: int) -> int:
         l = 0
         r = len(nums) - 1
 
-        while l < r:
+        while l <= r:
             mid = (l + r) // 2
+            
+            if nums[mid] == target:
+                return mid
 
-            if nums[mid] > nums[r]:
-                l = mid + 1
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
             else:
-                r = mid
+                if nums[mid] < target and target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
         
-        return nums[l]
+        return -1
