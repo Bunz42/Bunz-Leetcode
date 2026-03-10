5f6ebd77b24afbf9e5f229b76e35d2ced9e26605
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-03-10 15:15:47 -0400

    Add TimeMap class for time-based key-value storage
    Implemented a time-based key-value store with set and get methods using binary search.

--- neetcode-150/Binary Search/32-Time-Based-Key-Value-Store/main.py
+++ neetcode-150/Binary Search/32-Time-Based-Key-Value-Store/main.py
@@ -1,14 +1,31 @@
-class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        l = 0
-        r = len(nums) - 1
+class TimeMap:
 
-        while l < r:
+    def __init__(self):
+        self.time_map = collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.time_map[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.time_map:
+            return ""
+
+        values = self.time_map[key]
+
+        l, r = 0, len(values) - 1
+        res = ""
+
+        while l <= r:
             mid = (l + r) // 2
 
-            if nums[mid] > nums[r]:
+            mid_stamp = values[mid][1]
+            mid_value = values[mid][0]
+
+            if mid_stamp <= timestamp:
+                res = mid_value
                 l = mid + 1
             else:
-                r = mid
+                r = mid - 1
         
-        return nums[l]
+        return res
+
