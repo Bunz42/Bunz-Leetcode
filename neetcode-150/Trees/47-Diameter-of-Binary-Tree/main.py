583442f8ed88611660a6efec4ff7c2dd511a6d7b
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-03-15 19:36:25 -0400

    Add diameterOfBinaryTree method to calculate diameter
    Implement diameter calculation using depth-first search.

--- neetcode-150/Trees/47-Diameter-of-Binary-Tree/main.py
+++ neetcode-150/Trees/47-Diameter-of-Binary-Tree/main.py
@@ -6,11 +6,20 @@
 #         self.right = right
 
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
 
-        left = self.maxDepth(root.left)
-        right = self.maxDepth(root.right)
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
 
-        return 1 + max(left, right)
+            self.res = max(self.res, left + right)
+            
+            return 1 + max(left, right)
+        
+        dfs(root)
+
+        return self.res
