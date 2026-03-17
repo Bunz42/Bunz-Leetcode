6ad9bc046389bdf1834beee41690c4e97b8b882b
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-03-17 14:20:11 -0400

    Implement subtree check in main.py

--- neetcode-150/Trees/50-Subtree-of-Another-Tree/main.py
+++ neetcode-150/Trees/50-Subtree-of-Another-Tree/main.py
@@ -5,15 +5,27 @@
 #         self.left = left
 #         self.right = right
 
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def sameTree(r1, r2):
+            if not r1 and not r2:
+                return True
+            
+            if not r1 or not r2:
+                return False
+            
+            if r1.val != r2.val:
+                return False
+            
+            return sameTree(r1.left, r2.left) and sameTree(r1.right, r2.right)
+        
+        if not subRoot:
             return True
         
-        if not p or not q:
+        if not root:
             return False
         
-        if p.val != q.val:
-            return False
+        if sameTree(root, subRoot):
+            return True
         
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
